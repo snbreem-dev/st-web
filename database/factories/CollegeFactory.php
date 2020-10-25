cfcb941b9039f9dfe222495e3c6e4363882ed475
unknown <solimannb@gmail.com>
2020-10-25 18:31:07 +0200

    add college model

---
+++ database/factories/CollegeFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\College;
+use Faker\Generator as Faker;
+
+$factory->define(College::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
